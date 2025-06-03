31b136ab90af93ce9598392b32e38075e25ec08c
Divyansh Moonat <divyanshmoonat@outlook.com>
2025-06-03 21:50:54 +0530

    Intermeditate push : Project deployment

--- projects/job-app/index.js
+++ projects/job-app/index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 // Connection with MongoDB
 // mongodb://server:port/db_name
 mongoose
-    .connect("mongodb://127.0.0.1:27017/jobapp")
+    .connect("mongodb+srv://djain7429:Pa9F5adkSXR2NLJu@cluster0.wbipcdx.mongodb.net/")
     .then(() => console.log(`DB Connected successfully`))
     .catch(err => console.log(`Error connecting database`, err));
 
