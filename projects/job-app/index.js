1e78c3e1d1d7ea74586ff4cc1d2ded72a8650a2a
Divyansh Moonat <divyanshmoonat@outlook.com>
2025-06-05 20:24:26 +0530

    Intermeditate push : Project deployment

--- projects/job-app/index.js
+++ projects/job-app/index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
 const jobRoutes = require("./route/job.routes");
 
 const app = express();
+
+dotenv.config();
 
 // Middleware
 app.use(express.json());
@@ -11,14 +14,13 @@ app.use(express.json());
 // Connection with MongoDB
 // mongodb://server:port/db_name
 mongoose
-    .connect("mongodb+srv://djain7429:Pa9F5adkSXR2NLJu@cluster0.wbipcdx.mongodb.net/")
+    .connect(process.env.DB_URL)
     .then(() => console.log(`DB Connected successfully`))
     .catch(err => console.log(`Error connecting database`, err));
-
 
 // Routes
 app.use("/api/v1/job", jobRoutes);
 
-const portNo = 8080;
+const portNo = process.env.PORT;
 
 app.listen(portNo, () => console.log(`Server is up and running at port ${portNo}`));
